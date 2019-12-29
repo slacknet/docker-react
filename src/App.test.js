df788afb885034bf8a4b6160f1819b90d1edb838
cyberk <stefaniu.services@gmail.com>
2019-12-29 15:21:03 +0000

    change the test file

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/**test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});*/
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
