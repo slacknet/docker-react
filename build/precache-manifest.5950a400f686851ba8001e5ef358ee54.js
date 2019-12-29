2614d800b11158932b44663f2b74f9043a3f99ad
slacknet <stefaniu.services@gmail.com>
2019-12-29 00:10:24 +0000

    nfeijr

---
+++ build/precache-manifest.5950a400f686851ba8001e5ef358ee54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5339b73f3ce00863eff082c6810d30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ef206729d4858722218",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2b2f682da2d1daa3f730",
+    "url": "/static/js/2.29769018.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.29769018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ef206729d4858722218",
+    "url": "/static/js/main.0077a747.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
