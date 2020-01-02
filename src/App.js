1636e1f5c81fe8082916a6574aa020a6f52b998f
cyberk <stefaniu.services@gmail.com>
2020-01-02 18:45:01 +0000

    changes 6

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         Hello BINK team. This is the automated test where the following tech are involved:
+        <hr></hr>
           <li>Github repo, where I pushed all the app code</li>
           <li>Inside there is a docker file to build the app</li>
           <li>Travis CI - there is a RUN script to run the script</li>
