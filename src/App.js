7e2e46ef2896895ec9662666a0c36cf3e0f70f7c
cyberk <stefaniu.services@gmail.com>
2020-01-02 17:16:26 +0000

    changes2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn About this
+          Learn React
         </a>
       </header>
     </div>
